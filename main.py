cf0e95bbb736624666cafab99e4b307827ef4192
gtphani <gt.phani@belong.co>
2019-05-04 20:52:11 +0530

    basic functionality done

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from scraper import main
+from scraper import Scraper
 
 app = Flask(__name__)
 
@@ -12,10 +12,18 @@ def home():
 @app.route('/issues', methods=['POST', 'GET'])
 def result():
     if request.method == 'POST':
-        url = request.form
-        ctx = main(url)
+        url = request.form['repo_url']
+        scraper = Scraper(url)
+        if not scraper.check_validity():
+            return
+        scraper.scrape()
+        ctx = scraper.parse_and_render()
         return render_template("output.html", ctx=ctx)
 
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
+from collections import OrderedDict
 
 # data extraction classes in the html
 issues_entity = ['div', 'js-navigation-container js-active-navigation-container']
@@ -9,58 +10,59 @@ data_box = ['span', 'opened-by']
 date_format = "%Y-%m-%dT%H:%M:%SZ"
 
 
+class Scraper():
 
-def scraper(url):
+    def __init__(self, url):
+        self.url = url
+        self.issues = []
 
-    """
-    scrapes all the issues until the last page and returns list of all the issue blocks(html)
-    """
+    def check_validity(self):
+        if 'github.com' not in self.url:
+            return False
+        return True
 
-    issues = []
-    issues_data = True
-    i = 1
+    def scrape(self):
 
-    while issues_data:
-        print (i)
-        response = requests.get(url + "/issues?page={}&q=is%3Aissue+is%3Aopen".format(i))
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.content, 'html.parser')
-            issues_data = soup.find( issues_entity[0], {'class': issues_entity[1]})
-            if issues_data:
-                issues.extend(issues_data.find_all(issue_box[0], {'class': issue_box[1]}))
-                i += 1
+        """
+        scrapes all the issues until the last page and returns list of all the issue blocks(html)
+        """
+        issues_data = True
+        i = 1
 
-    return issues
+        while issues_data:
+            response = requests.get(self.url + "/issues?page={}&q=is%3Aissue+is%3Aopen".format(i))
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.content, 'html.parser')
+                issues_data = soup.find( issues_entity[0], {'class': issues_entity[1]})
+                if issues_data:
+                    self.issues.extend(issues_data.find_all(issue_box[0], {'class': issue_box[1]}))
+                    i += 1
 
+    def get_datetime(self, s):
+        s = str(s)
+        index = s.find("datetime=") + len("datetime=") + 1
+        return datetime.strptime(s[index:index + 20], date_format)
 
-def get_datetime(s):
-    s = str(s)
-    index = s.find("datetime=") + len("datetime=") + 1
-    return datetime.strptime(s[index:index + 20], date_format)
+    def parse_and_render(self):
+        ctx = OrderedDict()
+        if not self.issues:
+            return ctx
+        ctx['total'] = len(self.issues)
+        past_day = 0
+        past_week = 0
+        more_than_a_week = 0
+        now = datetime.now()
+        for issue in self.issues:
+            issue_time_stamp = self.get_datetime(issue.find(data_box[0], {'class': data_box[1]}))
+            time_passed = (now - issue_time_stamp).days
+            if time_passed == 0:
+                past_day += 1
+            elif 1 <= time_passed <= 7:
+                past_week += 1
+            elif time_passed > 7:
+                more_than_a_week += 1
+        ctx['past_day'] = past_day
+        ctx['past_week'] = past_week
+        ctx['more_than_a_week'] = more_than_a_week
 
-
-def parse_and_render(issues):
-    ctx = {}
-    ctx['total'] = len(issues)
-    past_day = 0
-    past_week = 0
-    more_than_a_week = 0
-    now = datetime.now()
-    for issue in issues:
-        issue_time_stamp = get_datetime(issue.find(data_box[0], {'class': data_box[1]}))
-        time_passed = (now - issue_time_stamp).days
-        if time_passed == 0:
-            past_day += 1
-        elif 1 <= time_passed <= 7:
-            past_week += 1
-        elif time_passed > 7:
-            more_than_a_week += 1
-    ctx['past_day'] = past_day
-    ctx['past_week'] = past_week
-    ctx['more_than_a_week'] = more_than_a_week
-    print (ctx)
-
-
-def main(url):
-    issues = scraper(url)
-    parse_and_render(issues)
+        return ctx
